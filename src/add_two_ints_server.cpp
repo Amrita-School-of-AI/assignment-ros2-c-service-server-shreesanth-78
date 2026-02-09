46b4bb3ad4ebf3ebfce9dabd39f60e43bd79ff06
shreesanth-78 <shreesanth78official@gmain.com>
2026-02-09 11:47:10 +0530

    Completed ROS2 Assignment service server

---
+++ src/add_two_ints_server.cpp
@@ -0,0 +1,43 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+class AddTwoIntsServer : public rclcpp::Node
+{
+public:
+  AddTwoIntsServer() : Node("add_two_ints_server")
+  {
+    service_ = this->create_service<example_interfaces::srv::AddTwoInts>(
+      "add_two_ints",
+      std::bind(&AddTwoIntsServer::handle_service, this, _1, _2)
+    );
+
+    RCLCPP_INFO(this->get_logger(), "AddTwoInts service server started");
+  }
+
+private:
+  void handle_service(
+    const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
+    std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
+  {
+    response->sum = request->a + request->b;
+    RCLCPP_INFO(this->get_logger(), "Incoming request: a=%ld b=%ld",
+                request->a, request->b);
+    RCLCPP_INFO(this->get_logger(), "Sending response: sum=%ld",
+                response->sum);
+  }
+
+  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service_;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<AddTwoIntsServer>());
+  rclcpp::shutdown();
+  return 0;
+}
