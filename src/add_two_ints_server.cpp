bccac5ee4cf8fb07103d1311d649eb523cc55eab
Shree Santh B <shreesanth78official@gmail.com>
2026-02-10 23:29:20 +0530

    Update add_two_ints_server.cpp

--- src/add_two_ints_server.cpp
+++ src/add_two_ints_server.cpp
@@ -1,48 +1,37 @@
-#include <memory>
-#include <functional>
-
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/srv/add_two_ints.hpp"
 
-using std::placeholders::_1;
-using std::placeholders::_2;
+using AddTwoInts = example_interfaces::srv::AddTwoInts;
 
 class AddTwoIntsServer : public rclcpp::Node
 {
 public:
-  AddTwoIntsServer() : Node("add_two_ints_server")
+  AddTwoIntsServer()
+  : Node("add_two_ints_server")
   {
-    service_ = this->create_service<example_interfaces::srv::AddTwoInts>(
+    service_ = this->create_service<AddTwoInts>(
       "add_two_ints",
-      std::bind(&AddTwoIntsServer::add, this, _1, _2));
-
-    RCLCPP_INFO(this->get_logger(), "AddTwoInts service is ready.");
+      std::bind(&AddTwoIntsServer::handle_add_two_ints, this, std::placeholders::_1, std::placeholders::_2)
+    );
   }
 
 private:
-  void add(
-    const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
-    std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response)
+  void handle_add_two_ints(
+    const std::shared_ptr<AddTwoInts::Request> request,
+    std::shared_ptr<AddTwoInts::Response> response)
   {
-    RCLCPP_INFO(this->get_logger(),
-      "Incoming request: a=%ld, b=%ld",
-      request->a, request->b);
-
     response->sum = request->a + request->b;
-
-    RCLCPP_INFO(this->get_logger(),
-      "Sending response: sum=%ld",
-      response->sum);
+    RCLCPP_INFO(this->get_logger(), "Incoming request: a=%ld b=%ld", request->a, request->b);
+    RCLCPP_INFO(this->get_logger(), "Sending response: sum=%ld", response->sum);
   }
 
-  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service_;
+  rclcpp::Service<AddTwoInts>::SharedPtr service_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<AddTwoIntsServer>());
   rclcpp::shutdown();
   return 0;
 }
-
